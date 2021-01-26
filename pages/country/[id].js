89efb82972b124e91db4b58a53cf65dc7bcce833
sujoyduttajad <sujoyduttajad@gmail.com>
2021-01-26 20:58:37 +0530

    getServerSide to getStaticProps

--- pages/country/[id].js
+++ pages/country/[id].js
@@ -123,16 +123,26 @@ const Country = ({ country }) => {
 
 export default Country;
 
-export const getServerSideProps = async ({ params }) => {
-    const res = await fetch(
-        `https://restcountries.eu/rest/v2/alpha/${params.id}`
-    );
-
-    const country = await res.json();
-
+export const getStaticPaths = async () => {
+    const res = await fetch("https://restcountries.eu/rest/v2/all");
+    const countries = await res.json();
+  
+    const paths = countries.map((country) => ({
+      params: { id: country.alpha3Code },
+    }));
+  
     return {
-        props: {
-            country,
-        },
-    }
-}
+      paths,
+      fallback: false,
+    };
+  };
+  
+  export const getStaticProps = async ({ params }) => {
+    const country = await getCountry(params.id);
+  
+    return {
+      props: {
+        country,
+      },
+    };
+  };
