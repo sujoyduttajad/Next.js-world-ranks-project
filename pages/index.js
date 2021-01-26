bd612173e8d82c35d7c6316986237c81d03e7a05
sujoyduttajad <sujoyduttajad@gmail.com>
2021-01-26 20:23:51 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
-import Head from 'next/head'
+import React, {useState} from 'react';
 import styles from '../styles/Home.module.css'
+import Layout from '../src/components/Layout/Layout'
+import SearchInput from '../src/components/SearchInput/SearchInput'
+import CountriesTable from '../src/components/CountriesTable/CountriesTable';
 
-export default function Home() {
+
+export default function Home({countries}) {
+  
+  const [keyword, setKeyword] = useState('');
+
+  const filteredCountries = countries.filter((country) => 
+    country.name.toLowerCase().includes(keyword) ||
+    country.region.toLowerCase().includes(keyword) ||
+    country.subregion.toLowerCase().includes(keyword) 
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+      <SearchInput 
+        placeholder="Filter by Name, Region or Subregion"
+        onChange={onInputChange} />
+      <CountriesTable countries={filteredCountries}/>
+    
+    </Layout>
   )
 }
+
+export const getStaticProps = async () => {
+  
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+  
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
+
