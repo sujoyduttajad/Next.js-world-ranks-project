a8532cbf4e870d4b34ef77da0c3965ba295d9b91
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-29 13:07:48 +0530

    Update SearchInput.js

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,14 +1,13 @@
 import SearchRounded from "@material-ui/icons/SearchRounded";
-import styles from './SearchInput.module.css';
+import styles from "./SearchInput.module.css";
 
-
-const SearchInput = ({...rest}) => {
-    return (
+const SearchInput = ({ ...rest }) => {
+  return (
     <div className={styles.wrapper}>
-        <SearchRounded color="inherit"/>
-        <input className={styles.input} {...rest} />
+      <SearchRounded color="inherit" />
+      <input className={styles.input} {...rest} />
     </div>
-    )
-} 
+  );
+};
 
 export default SearchInput;
